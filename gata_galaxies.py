8e9e10107a62a97f5311e5bda26cb7f256dead0d
Emilie Menard Barnard <emiliebarnard@gmail.com>
2022-03-06 02:52:14 -0800

    Need to adjust for bases other than 2

--- gata_galaxies.py
+++ gata_galaxies.py
@@ -5,9 +5,12 @@ HEIGHT = 7
 import random
 
 def main():
-    vector0_test = [0] * WIDTH
-    rule_test = 30
-    print(gen_table(rule_test))
+    vector0_test = [1, 0, 0, 0, 0, 0, 0, 0, 0]
+    rule_test = 111
+
+    lookup_table = gen_table(rule_test)
+    display_vectors(ca_vector_gen(vector0_test, lookup_table))
+
 
 # convert rule number in decimal to base BASE_VAL, thanks stackoverflow :)
 # https://stackoverflow.com/questions/2267362/how-to-convert-an-integer-to-a-string-in-any-base
@@ -31,11 +34,28 @@ def gen_table(rule_num):
         return [0] * (8 - len(converted_num)) + converted_num
 
 # generate the vectors based on CA encryption
-def vector_gen(init_vector, rule_number):
-    for i in range(WIDTH):
-        x = 0
+def ca_vector_gen(init_vector, lookup_table):
+    vectors = []
+    prev_vector = init_vector
+    for vector_num in range(HEIGHT):
+        new_vector = []
+        #iterate through the numbers in the vector and update the values
+        for vector_elem_index in range(WIDTH): # assumes init_vector is of length WIDTH
+            # need to pull info from neighbor + current cells to apply the ca ecryption -
+            # grab indices, deal with wrap-around
+            p_index = (vector_elem_index - 1) % WIDTH # left
+            q_index = vector_elem_index % WIDTH # center
+            r_index = (vector_elem_index + 1) % WIDTH # right
+            # grab decimal representation of the value from p, q, r
+            dec_rep = 4 * prev_vector[p_index] + 2 * prev_vector[q_index] + prev_vector[r_index] # 2^2, 2^1, 2^0
+            # generate new cell based on 8-bit lookup table:
+            new_vector.append(lookup_table[7 - dec_rep])
+        vectors.append(new_vector)
+        prev_vector = new_vector
+    return vectors
 
-
-
+def display_vectors(vectors_2d):
+    for vector in vectors_2d:
+        print(' '.join(map(str, vector)))
 
 main()
