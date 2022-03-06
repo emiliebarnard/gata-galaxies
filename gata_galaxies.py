250bbb2f951d7d0f92290443a2f3a542447e3a17
Emilie Menard Barnard <emiliebarnard@gmail.com>
2022-03-06 03:19:46 -0800

    Lookup table working with any base

--- gata_galaxies.py
+++ gata_galaxies.py
@@ -6,11 +6,9 @@ import random
 
 def main():
     vector0_test = [1, 0, 0, 0, 0, 0, 0, 0, 0]
-    rule_test = 111
-
+    rule_test = 124
     lookup_table = gen_table(rule_test)
     display_vectors(ca_vector_gen(vector0_test, lookup_table))
-
 
 # convert rule number in decimal to base BASE_VAL, thanks stackoverflow :)
 # https://stackoverflow.com/questions/2267362/how-to-convert-an-integer-to-a-string-in-any-base
@@ -23,15 +21,15 @@ def conv_number(dec_num):
         dec_num //= BASE_VAL
     return digits[::-1]
 
-# creates 8-bit lookup table based on the rule number
+# creates lookup table based on the rule number
 def gen_table(rule_num):
     # first convert number to base representation
     converted_num = conv_number(rule_num)
     # then generate the 8-bit lookup table
-    if len(converted_num) >= 8:
-        return converted_num[:8]
+    if len(converted_num) >= BASE_VAL ** 3:
+        return converted_num[:BASE_VAL ** 3]
     else: # pad it with leading zeros
-        return [0] * (8 - len(converted_num)) + converted_num
+        return [0] * (BASE_VAL ** 3 - len(converted_num)) + converted_num
 
 # generate the vectors based on CA encryption
 def ca_vector_gen(init_vector, lookup_table):
@@ -46,10 +44,11 @@ def ca_vector_gen(init_vector, lookup_table):
             p_index = (vector_elem_index - 1) % WIDTH # left
             q_index = vector_elem_index % WIDTH # center
             r_index = (vector_elem_index + 1) % WIDTH # right
+            # generate new cell based on lookup table:
             # grab decimal representation of the value from p, q, r
-            dec_rep = 4 * prev_vector[p_index] + 2 * prev_vector[q_index] + prev_vector[r_index] # 2^2, 2^1, 2^0
-            # generate new cell based on 8-bit lookup table:
-            new_vector.append(lookup_table[7 - dec_rep])
+            dec_rep = BASE_VAL**2 * prev_vector[p_index] + BASE_VAL * prev_vector[q_index] + prev_vector[r_index] # base^2, base^1, base^0
+            max_val = BASE_VAL ** 2 * (BASE_VAL - 1) + BASE_VAL * (BASE_VAL - 1) + (BASE_VAL - 1)
+            new_vector.append(lookup_table[BASE_VAL ** 3 - 1 - dec_rep])
         vectors.append(new_vector)
         prev_vector = new_vector
     return vectors
